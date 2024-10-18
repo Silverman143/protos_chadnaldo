fc65683cda5c979c0422f1535cae5e51277a44d7
Silverman143 <luckbrovova@gmail.com>
2024-10-18 16:49:13 +0400

    Add: Payments: Handle telegram successful payment

--- gen/go/payment/payment_grpc.pb.go
+++ gen/go/payment/payment_grpc.pb.go
@@ -19,10 +19,11 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	PaymentService_GetPaymentMethods_FullMethodName              = "/payment.PaymentService/GetPaymentMethods"
-	PaymentService_GetInvoiceLink_FullMethodName                 = "/payment.PaymentService/GetInvoiceLink"
-	PaymentService_GetPaymentStatus_FullMethodName               = "/payment.PaymentService/GetPaymentStatus"
-	PaymentService_HandleTelegramPreCheckoutQuery_FullMethodName = "/payment.PaymentService/HandleTelegramPreCheckoutQuery"
+	PaymentService_GetPaymentMethods_FullMethodName               = "/payment.PaymentService/GetPaymentMethods"
+	PaymentService_GetInvoiceLink_FullMethodName                  = "/payment.PaymentService/GetInvoiceLink"
+	PaymentService_GetPaymentStatus_FullMethodName                = "/payment.PaymentService/GetPaymentStatus"
+	PaymentService_HandleTelegramPreCheckoutQuery_FullMethodName  = "/payment.PaymentService/HandleTelegramPreCheckoutQuery"
+	PaymentService_HandleTelegramSuccessfulPayment_FullMethodName = "/payment.PaymentService/HandleTelegramSuccessfulPayment"
 )
 
 // PaymentServiceClient is the client API for PaymentService service.
@@ -32,7 +33,9 @@ type PaymentServiceClient interface {
 	GetPaymentMethods(ctx context.Context, in *GetPaymentMethodsRequest, opts ...grpc.CallOption) (*GetPaymentMethodsResponse, error)
 	GetInvoiceLink(ctx context.Context, in *GetInvoiceLinkRequest, opts ...grpc.CallOption) (*GetInvoiceLinkResponse, error)
 	GetPaymentStatus(ctx context.Context, in *GetPaymentStatusRequest, opts ...grpc.CallOption) (*GetPaymentStatusResponse, error)
+	// Telegram handlers
 	HandleTelegramPreCheckoutQuery(ctx context.Context, in *HandleTelegramPreCheckoutQueryRequest, opts ...grpc.CallOption) (*HandleTelegramPreCheckoutQueryResponse, error)
+	HandleTelegramSuccessfulPayment(ctx context.Context, in *HandleTelegramSuccessfulPaymentRequest, opts ...grpc.CallOption) (*HandleTelegramSuccessfulPaymentResponse, error)
 }
 
 type paymentServiceClient struct {
@@ -83,6 +86,16 @@ func (c *paymentServiceClient) HandleTelegramPreCheckoutQuery(ctx context.Contex
 	return out, nil
 }
 
+func (c *paymentServiceClient) HandleTelegramSuccessfulPayment(ctx context.Context, in *HandleTelegramSuccessfulPaymentRequest, opts ...grpc.CallOption) (*HandleTelegramSuccessfulPaymentResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(HandleTelegramSuccessfulPaymentResponse)
+	err := c.cc.Invoke(ctx, PaymentService_HandleTelegramSuccessfulPayment_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // PaymentServiceServer is the server API for PaymentService service.
 // All implementations must embed UnimplementedPaymentServiceServer
 // for forward compatibility.
@@ -90,7 +103,9 @@ type PaymentServiceServer interface {
 	GetPaymentMethods(context.Context, *GetPaymentMethodsRequest) (*GetPaymentMethodsResponse, error)
 	GetInvoiceLink(context.Context, *GetInvoiceLinkRequest) (*GetInvoiceLinkResponse, error)
 	GetPaymentStatus(context.Context, *GetPaymentStatusRequest) (*GetPaymentStatusResponse, error)
+	// Telegram handlers
 	HandleTelegramPreCheckoutQuery(context.Context, *HandleTelegramPreCheckoutQueryRequest) (*HandleTelegramPreCheckoutQueryResponse, error)
+	HandleTelegramSuccessfulPayment(context.Context, *HandleTelegramSuccessfulPaymentRequest) (*HandleTelegramSuccessfulPaymentResponse, error)
 	mustEmbedUnimplementedPaymentServiceServer()
 }
 
@@ -112,6 +127,9 @@ func (UnimplementedPaymentServiceServer) GetPaymentStatus(context.Context, *GetP
 }
 func (UnimplementedPaymentServiceServer) HandleTelegramPreCheckoutQuery(context.Context, *HandleTelegramPreCheckoutQueryRequest) (*HandleTelegramPreCheckoutQueryResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method HandleTelegramPreCheckoutQuery not implemented")
+}
+func (UnimplementedPaymentServiceServer) HandleTelegramSuccessfulPayment(context.Context, *HandleTelegramSuccessfulPaymentRequest) (*HandleTelegramSuccessfulPaymentResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method HandleTelegramSuccessfulPayment not implemented")
 }
 func (UnimplementedPaymentServiceServer) mustEmbedUnimplementedPaymentServiceServer() {}
 func (UnimplementedPaymentServiceServer) testEmbeddedByValue()                        {}
@@ -206,6 +224,24 @@ func _PaymentService_HandleTelegramPreCheckoutQuery_Handler(srv interface{}, ctx
 	return interceptor(ctx, in, info, handler)
 }
 
+func _PaymentService_HandleTelegramSuccessfulPayment_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(HandleTelegramSuccessfulPaymentRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PaymentServiceServer).HandleTelegramSuccessfulPayment(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: PaymentService_HandleTelegramSuccessfulPayment_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PaymentServiceServer).HandleTelegramSuccessfulPayment(ctx, req.(*HandleTelegramSuccessfulPaymentRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // PaymentService_ServiceDesc is the grpc.ServiceDesc for PaymentService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -228,6 +264,10 @@ var PaymentService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "HandleTelegramPreCheckoutQuery",
 			Handler:    _PaymentService_HandleTelegramPreCheckoutQuery_Handler,
+		},
+		{
+			MethodName: "HandleTelegramSuccessfulPayment",
+			Handler:    _PaymentService_HandleTelegramSuccessfulPayment_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
